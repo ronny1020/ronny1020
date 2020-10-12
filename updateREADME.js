17e083ddf97d09653119db42566293769959bb7d
Ronny Hsieh <ronny_hsieh@systemweb.com.tw>
2020-10-12 16:39:19 +0800

    write readme

--- updateREADME.js
+++ updateREADME.js
@@ -17,81 +17,24 @@ async function updateProfile() {
 
   lastRepos.forEach((item) => {
     lastReposString += `[![ReadMe Card](https://github-readme-stats.vercel.app/api/pin/?username=ronny1020&repo=${item.name})](${item.html_url})
+
 `
   })
 
   const mainProfile = fs.readFileSync('./mainProfile.md', 'utf8')
 
-  console.log(lastReposString)
-  console.log(mainProfile)
+  const readme = `${mainProfile}
+## Last repositories
+
+${lastReposString.trim()}`
+
+  fs.writeFile('README.md', readme, 'utf-8', (err) => {
+    if (err) {
+      throw err
+    }
+
+    console.log('README update complete.')
+  })
 }
 
 updateProfile()
-
-// new Promise((resolve, reject) => {
-
-// })
-//   .then((resolve, reject) => {
-//     const url = 'https://api.github.com/users/ronny1020/repos'
-//     https.get(url, (resp) => {
-//       let data = ''
-
-//       // A chunk of data has been recieved.
-//       resp.on('data', (chunk) => {
-//         data += chunk
-//       })
-
-//       // The whole response has been received. Print out the result.
-//       resp.on('end', () => {
-//         console.log(JSON.parse(data))
-//       })
-//     })
-
-//     // console.log(resolve)
-//   })
-//   .catch((error) => {
-//     console.log(error)
-//   })
-
-// // API
-//
-
-// https.get(url, (res) => {
-//   res.setEncoding('utf8')
-
-//   res.on('data', (data) => (body += data))
-
-//   res.on('end', () => {
-//     // Parse the JSON response
-//     body = JSON.parse(body)
-
-//     // Shorten array to latest 3 articles
-//     body = body.slice(0, 3)
-
-//     // Create string of markdown to be inserted
-//     const articles = `\n - [${body[0].title}](${body[0].url})\n - [${body[1].title}](${body[1].url})\n - [${body[2].title}](${body[2].url})\n \n`
-
-//     // Update README using FS
-//     fs.readFile('README.md', 'utf-8', (err, data) => {
-//       if (err) {
-//         throw err
-//       }
-
-//       // Replace text using regex: "I'm writing: ...replace... ![Build"
-//       // Regex101.com is a lifesaver!
-//       const updatedMd = data.replace(
-//         /(?<=I'm writing:\n)[\s\S]*(?=\!\[Build)/gim,
-//         articles
-//       )
-
-//       // Write the new README
-//       fs.writeFile('README.md', updatedMd, 'utf-8', (err) => {
-//         if (err) {
-//           throw err
-//         }
-
-//         console.log('README update complete.')
-//       })
-//     })
-//   })
-// })
